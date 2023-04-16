b270e8077fae9f1de3f6b81ca3a1da430028a8a8
Hyunyeop Ahn <hyeop9@gmail.com>
2023-04-16 17:23:36 +0900

    solve : max min

--- programmers/src/level2/MaxMin.java
+++ programmers/src/level2/MaxMin.java
@@ -1,11 +1,24 @@
 package level2;
 
 /**
- *  문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다.
- *  str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
- *  예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+ * 문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요. 예를들어
+ * s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
  */
 
 public class MaxMin {
-
+    String solution(String s) {
+        String[] tmp = s.split(" ");
+        int min, max, n;
+        min = max = Integer.parseInt(tmp[0]);
+        for (int i = 1; i < tmp.length; i++) {
+            n = Integer.parseInt(tmp[i]);
+            if (min > n) {
+                min = n;
+            }
+            if (max < n) {
+                max = n;
+            }
+        }
+        return min + " " + max;
+    }
 }
