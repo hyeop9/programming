9cec7af85a0698a0e851fe3d3a7caa2fd43f0879
Hyunyeop Ahn <hyeop9@gmail.com>
2023-07-02 18:19:33 +0900

    Update 1

---
+++ java-for-coding-test/src/DFS_BFS/FreezingDrinks.java
@@ -0,0 +1,70 @@
+package DFS_BFS;
+
+import java.util.Scanner;
+
+/**
+ * 음료수 얼려 먹기
+ * N x M 크기의 얼음 틀이 있다. 구멍이 뚫려 있는 부분은 0, 칸막이가 존재하는 부분은 1로 표시된다.
+ * 구멍이 뚫려 있는 부분끼리 상, 하, 좌, 우로 붙어 있는 경우 서로 연결되어 있는 것으로 간주한다.
+ * 이때 얼음 틀의 모양이 주어졌을 때 생성되는 총 아이스크림의 개수를 구하는 프로그램을 작성하시오.
+ * <p>
+ * 입력 조건:
+ * 첫 번쨰 줄에 얼음 틀의 세로 길이 N과 가로 길이 M이 주어진다.
+ * 두 번째 줄부터 N + 1 번째 줄까지 얼음 틀의 형태가 주어진다.
+ * 이때 구멍이 뚫려 있는 부분은 0, 그렇지 않은 부분은 1이다.
+ */
+
+public class FreezingDrinks {
+
+    public static int n, m;
+    public static int[][] graph = new int[1000][1000];
+
+    // DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+    public static boolean dfs(int x, int y) {
+        // 주어진 범위를 벗어나는 경우에는 즉시 종료
+        if (x <= -1 || x >= n || y <= -1 || y >= m) {
+            return false;
+        }
+        // 현재 노드를 아직 방문하지 않았다면
+        if (graph[x][y] == 0) {
+            // 해당 노드 방문 처리
+            graph[x][y] = 1;
+            // 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+            dfs(x - 1, y);
+            dfs(x, y - 1);
+            dfs(x + 1, y);
+            dfs(x, y + 1);
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // N, M을 공백으로 기준으로 구분하여 입력 받기
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.nextLine(); // 버퍼 지우기
+
+        // 2차원 리스트의 맵 정보 입력 받기
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        // 모든 노드(위치)에 대하여 음료수 채우기
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // 현재 위치에서 DFS 수행
+                if (dfs(i, j)) {
+                    result += 1;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
