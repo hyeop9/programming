fbed887ec465710ba3a1820e352ede141c914225
Hyunyeop Ahn <hyeop9@gmail.com>
2023-07-02 20:09:56 +0900

    Update 2

---
+++ java-for-coding-test/src/sort/LowGrade.java
@@ -0,0 +1,63 @@
+package sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * N명의 학생 정보가 있다. 학생 정보는 학생의 이름과 학생의 성적으로 구분된다.
+ * 각 학생의 이름과 성적 정보가 주어졌을 때 성적이 낮은 순서대로 학생의 이름을 출력하는 프로그램을 작성하시오.
+ * <p>
+ * 입력 조건:
+ * 첫 번째 줄에 학생의 수 N이 입력된다.
+ * 두 번째 줄부터 N + 1번째 줄에는 학생의 이름을 나타내는 문자열 A와 학생의 성적을 나타내는 정수 B가 공백으로 구분되어 입력된다.
+ */
+
+class Student implements Comparable<Student> {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    // 정렬 기준은 '점수가 낮은 순서
+    @Override
+    public int compareTo(Student other) {
+        if (this.score < other.score) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
+public class LowGrade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Student> students = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int score = sc.nextInt();
+            students.add(new Student(name, score));
+        }
+
+        Collections.sort(students);
+
+        for (int i = 0; i < students.size(); i++) {
+            System.out.print(students.get(i).getName() + " ");
+        }
+    }
+}
